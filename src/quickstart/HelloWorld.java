9efabdf579eddc9bcbcc4578c07a1ca951cb9d70
Christian Brouns <christian.brouns@gmail.com>
2015-12-15 17:49:16 +0100

    HelloWorld1

--- src/quickstart/HelloWorld.java
+++ src/quickstart/HelloWorld.java
@@ -4,7 +4,8 @@ package quickstart;
  * Created by Christian on 14-12-2015.
  */
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
 }
